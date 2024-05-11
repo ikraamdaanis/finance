1cf9b52f983d741971967004d045e7d297456451
Ikraam <ibndaanis@gmail.com>
2024-05-11 22:32:42 +0100

    sign up page

--- app/(auth)/sign-up/[[...signUp]]/page.tsx
+++ app/(auth)/sign-up/[[...signUp]]/page.tsx
@@ -1,5 +1,27 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 export default async function SignUpPage() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <div className="grid min-h-screen grid-cols-1 lg:grid-cols-2">
+      <div className="h-full flex-col items-center justify-center px-4 lg:flex">
+        <div className="space-y-4 pt-16 text-center">
+          <h1 className="text-3xl font-bold text-[#2a2a47]">Welcome Back</h1>
+          <p className="text-base text-[#7e8ca0]">Create account or log in!</p>
+        </div>
+        <div className="mt-8 flex items-center justify-center">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="hidden h-full items-center justify-center bg-blue-600 lg:flex">
+        <Image src="/logo.svg" height={100} width={100} alt="Logo" />
+      </div>
+    </div>
+  );
 }
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
     <div>
-      <Button>Click Me</Button>
+      <UserButton />
     </div>
   );
 }
