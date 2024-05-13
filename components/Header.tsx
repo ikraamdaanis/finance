5298a7588480e852a025790d56cec0d37bc9e431
Ikraam <ibndaanis@gmail.com>
2024-05-13 02:50:39 +0100

    welcome message

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 import { HeaderLogo } from "components/HeaderLogo";
 import { Navigation } from "components/Navigation";
+import { WelcomeMessage } from "components/WelcomeMessage";
 
 export const Header = () => {
   return (
@@ -10,7 +12,14 @@ export const Header = () => {
             <HeaderLogo />
             <Navigation />
           </div>
+          <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <div className="size-7 animate-pulse rounded-full bg-slate-400" />
+          </ClerkLoading>
         </div>
+        <WelcomeMessage />
       </div>
     </header>
   );
