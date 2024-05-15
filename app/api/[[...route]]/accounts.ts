318e2b6a27ada2233d9f85cd961442ee90072883
Ikraam <ibndaanis@gmail.com>
2024-05-16 00:00:07 +0100

    accounts userid filter

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { db } from "db/drizzle";
 import { accounts } from "db/schema";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
@@ -13,7 +14,8 @@ const app = new Hono().get("/", clerkMiddleware(), async c => {
 
   const data = await db
     .select({ id: accounts.id, name: accounts.name })
-    .from(accounts);
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
   return c.json({ data });
 });
