baa0a321cd0e99db000cff70b7b495dfa468cace
Ikraam <ibndaanis@gmail.com>
2024-06-09 14:25:29 +0100

    removed unused code

--- app/api/[[...route]]/app.ts
+++ app/api/[[...route]]/app.ts
@@ -4,29 +4,18 @@ import { accounts } from "db/schema";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { zValidator } from "./accounts";
 
-export const app = new Hono()
-  .get("/", clerkMiddleware(), async c => {
-    const auth = getAuth(c);
+export const app = new Hono().get("/", clerkMiddleware(), async c => {
+  const auth = getAuth(c);
 
-    if (!auth?.userId) {
-      throw new HTTPException(401, { res: c.json({ error: "unauthorized" }) });
-    }
+  if (!auth?.userId) {
+    throw new HTTPException(401, { res: c.json({ error: "unauthorized" }) });
+  }
 
-    const data = await db
-      .select({ id: accounts.id, name: accounts.name })
-      .from(accounts)
-      .where(eq(accounts.userId, auth.userId));
+  const data = await db
+    .select({ id: accounts.id, name: accounts.name })
+    .from(accounts)
+    .where(eq(accounts.userId, auth.userId));
 
-    return c.json({ data });
-  })
-  .post("/", clerkMiddleware(), zValidator(), async c => {
-    const auth = getAuth(c);
-
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
-
-    return c.json({});
-  });
+  return c.json({ data });
+});
